ed4f3b60cf2d6f3cbe4bab25ff5e4310b0ab25ab
Dipali <dipssonawane123@gmail.com>
2022-07-24 22:15:48 +0530

    Mdified:primium rides feature is modified

--- src/main/java/com/cabinvoicegenerator/Ride.java
+++ src/main/java/com/cabinvoicegenerator/Ride.java
@@ -1,28 +1,38 @@
 package com.cabinvoicegenerator;
 
 public class Ride {
-    private double distance;
-    private int time;
+        private String rideType;
+        private double distance;
+        private int time;
 
-    /**
-     * to accept multiple rides
-     * @param distance
-     * @param time
-     */
-    public Ride(double distance, int time) {
-        this.distance = distance;
-        this.time = time;
+        public Ride(double distance, int time, String rideType) {
+            this.distance = distance;
+            this.time = time;
+            this.rideType = rideType;
+        }
+
+        public double getDistance() {
+            return distance;
+        }
+
+        public void setDistance(double distance) {
+            this.distance = distance;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        public void setTime(int time) {
+            this.time = time;
+        }
+
+        public String getRideType() {
+            return rideType;
+        }
+
+        public void setRideType(String rideType) {
+            this.rideType = rideType;
+        }
     }
 
-    public double getDistance() {
-        return distance;
-    }
-
-
-
-  public int getTime() {
-        return time;
-    }
-
-
-    }
